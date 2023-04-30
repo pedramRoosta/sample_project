4ca79fd5b2a3445b7c9ddcd9a159158339e1ce68
Pedram Roosta <pedram.roosta@bahag.com>
2023-04-30 18:44:44 +0200

    - add home.dart - change main from container to home

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 200,
+      child: Text('asadasd'),
+    );
+  }
+}
