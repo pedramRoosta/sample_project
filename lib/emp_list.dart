6fe1d9bd4309a8c7183026f6ee992ce8017c9a93
Pedram Roosta <pedram.roosta@bahag.com>
2023-04-30 19:09:31 +0200

    implement lists

---
+++ lib/emp_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Lists extends StatefulWidget {
+  const Lists({super.key});
+
+  @override
+  State<Lists> createState() => _ListsState();
+}
+
+class _ListsState extends State<Lists> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
